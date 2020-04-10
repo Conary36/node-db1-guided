37486e76c182f0141b934f384ea75a8e7e5d0c9e
Conary36 <cbeckford144@gmail.com>
2020-04-09 20:13:01 -0400

    added Delete

--- posts/post-router.js
+++ posts/post-router.js
@@ -60,7 +60,21 @@ router.put('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-
+    const {id} = req.params;
+    const item = req.body;
+    db('posts')
+        .where({id})
+        .del(item)
+        .then(e =>{
+            if(e > 0 ){
+                res.status(200).json({message: 'Deleted!'})
+            }else{
+                res.status(404).json({message: 'Not Found!'})//always include 404 to ensure the item does exist or not.
+            }
+        })
+        .catch(err => {
+            res.status(500).json({message: 'No luck, Try again!'})
+        })
 });
 
 module.exports = router;
